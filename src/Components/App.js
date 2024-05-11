243602da161ece2c45360f09bf8507bdc9cfab21
nohren <orendnelson@gmail.com>
2024-05-10 23:39:56 -0700

    web app new style

--- src/Components/App.js
+++ src/Components/App.js
@@ -6,13 +6,31 @@ import SearchBox from './SearchBox';
 import { Outlet } from 'react-router-dom';
 import { menuItems } from '../store/store';
 
+const pic =
+  'https://github.com/nohren/memes-table/blob/main/public/images/memes-table.jpeg?raw=true';
+
 const AppContainer = styled.div``;
 const BackgroundImageContainer = styled.div`
-  background-image: url('https://github.com/nohren/memes-table/blob/main/public/images/memes-table.jpeg?raw=true');
+  background-image: url('${({ url }) => url}');
   background-size: cover;
   background-position: center;
   height: 250px;
+  filter: drop-shadow(1px 10px 9px black);
 `;
+const BackgroundContainerBottom = styled.div`
+  background: linear-gradient(to top, #1c2e3b, transparent),
+    linear-gradient(to bottom, aqua, transparent);
+  background-position: top, bottom;
+  height: 100px;
+`;
+
+const BackgroundContainerTop = styled.div`
+  background: linear-gradient(to bottom, #1c2e3b, transparent),
+    linear-gradient(to top, aqua, transparent);
+  background-position: top, bottom;
+  height: 100px;
+`;
+
 const ViewContainer = styled.div``;
 const Centered = styled.h1`
   text-align: center;
@@ -29,7 +47,9 @@ export default function App() {
         <NavMenu menuItems={menuItems} />
         <SearchBox />
       </Menu>
-      <BackgroundImageContainer></BackgroundImageContainer>
+      <BackgroundContainerTop></BackgroundContainerTop>
+      <BackgroundImageContainer url={pic}></BackgroundImageContainer>
+      <BackgroundContainerBottom></BackgroundContainerBottom>
       <ViewContainer>
         <Outlet />
       </ViewContainer>
